23daf8e86a640213f7e60cc638b30068cf39cade
Andy-K-Taua <andykalasinitaua@gmail.com>
2026-05-28 11:07:59 +1000

    Fix: Remove hardcoded Stripe API version

--- backend/lib/stripe.js
+++ backend/lib/stripe.js
@@ -4,14 +4,10 @@ import Stripe from 'stripe';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// 1. Export the webhook secret for use in the controller
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
 
-// 2. Export the initialized Stripe instance as 'stripe'
-// Make sure this line exists!
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2025-02-24',
-});
+// Update this section:
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); 
 
 export const getStripe = () => {
     return stripe;
